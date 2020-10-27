6ad2b63bb154d667e6530c721cb320f80ddc85b9
Max Rangel <lik4ever@hotmail.com>
2020-10-26 23:33:01 -0600

    saving data in csv file

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+import csv
+
 # Needed to render static files
 app = Flask(__name__,
             static_url_path='/static',
@@ -16,9 +18,33 @@ def html_page(page_name=None):
     return render_template(f'{page_name}.html')
 
 
+# Saving data on txt file
+# def save_contact_data(data):
+#     with open('database.txt', mode="a") as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = database.write(f'\n{email},{subject},{message}')
+
+# Saving data on csv file
+def save_contact_data(data):
+    with open('database.csv', mode="a", newline='') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
 @app.route('/submit-form', methods=['POST', 'GET'])
 def form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-
-        return redirect('/thankyou')
+        try:
+            data = request.form.to_dict()
+            save_contact_data(data)
+            return redirect('/thankyou')
+        except:
+            return 'Did not save to database'
+    else:
+        return 'Something went very wrong!'
